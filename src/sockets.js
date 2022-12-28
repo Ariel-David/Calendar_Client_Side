f584b0787596591c81c11b94b0e7cf1301d98d69
Ariel <arieldavid642@gmail.com>
2022-12-28 14:40:01 +0200

    realtime update

--- src/sockets.js
+++ src/sockets.js
@@ -1,6 +1,7 @@
 import * as SockJS from "sockjs-client";
 import { Stomp } from "@stomp/stompjs";
 import { serverAddress } from "./constants";
+import {fillCalendar} from "./archive";
 
 let stompClient;
 const socketFactory = () => {
@@ -12,16 +13,30 @@ const onMessageReceived = (payload) => {
   console.log(payload.body);
 };
 
-const onConnected = (email) => {
-  console.log("on connected");
-  stompClient.subscribe("/notifications/" + email, onMessageReceived);
+const onUpdateReceived = (payload, key) => {
+  console.log("payload: ", payload.body);
+  console.log("key: ", key);
+
+  if(payload.body == "Event"){
+    fillCalendar(key);
+  }
+  else{
+    console.log("hi");
+  }
 };
 
-const openConnection = (email) => {
+const onConnected = (email,key) => {
+  console.log("keyyyyyyyy: ", key);
+  console.log("on connected");
+  stompClient.subscribe("/notifications/" + email, onMessageReceived);
+  stompClient.subscribe("/realTime/" + email, (payload) => {onUpdateReceived(payload, key);});
+};
+
+const openConnection = (email,key) => {
   console.log("on open connected");
   const socket = socketFactory();
   stompClient = Stomp.over(socket);
-  stompClient.connect({}, () => {onConnected(email)});
+  stompClient.connect({}, () => {onConnected(email,key)});
 };
 
 export { openConnection };
